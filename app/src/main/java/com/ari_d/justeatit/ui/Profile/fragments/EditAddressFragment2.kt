e1ee7bf02637a6bc71b55135e05da9f6a51c9dac
Ari_D <aridcompanysite@gmail.com>
2022-01-31 14:03:52 -0800

    designed layouts for address fragments

--- app/src/main/java/com/ari_d/justeatit/ui/Profile/fragments/EditAddressFragment2.kt
+++ app/src/main/java/com/ari_d/justeatit/ui/Profile/fragments/EditAddressFragment2.kt
@@ -5,7 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.ari_d.justeatit.R
 
-class AddressBook_Fragment: Fragment(R.layout.fragment_address_book) {
+class EditAddressFragment2: Fragment(R.layout.fragment_edit_address2) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
